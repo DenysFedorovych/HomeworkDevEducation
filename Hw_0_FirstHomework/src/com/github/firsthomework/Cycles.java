1567d2785d3502df50f620ff6ed50c2a2c63b6f9
denisfedorovich <fedororvich.d.d@gmail.com>
2021-02-01 14:39:54 +0200

    Adding more tests (Cycles)

--- Hw_0_FirstHomework/src/com/github/firsthomework/Cycles.java
+++ Hw_0_FirstHomework/src/com/github/firsthomework/Cycles.java
@@ -1,4 +1,69 @@
 package com.github.firsthomework;
 
 public class Cycles {
+    public static int sumOfEvenNumber(){
+            int sum=0;
+            for(int i=2;i<100;i+=2){
+                sum+=i;
+            }
+            return sum;
+    }
+
+    public static boolean isPrime(int a){
+        a = Math.abs(a);
+        for(int i=2;i<=Math.sqrt(a);i++){
+            if(a%i==0) return false;
+        }
+        return true;
+    }
+
+    public static int squareRootSimple(int a){
+        if(a<0) throw new IllegalArgumentException("Only positive numbers");
+        for(int i=0;i<=a;i++){
+            if(i*i>a) return i-1;
+            else{
+                if(i*i==a) return i;
+            }
+        }
+        return 0;
+    }
+
+    public static int squareRootBinary(int a){
+        if(a<0) throw new IllegalArgumentException("Only positive numbers");
+        int min=0;
+        int max=a+1;
+        int mid=0;
+        while(max-min>1){
+            mid = (min+max)/2;
+            if(mid*mid>a) max = mid;
+            else min = mid;
+        }
+        return min;
+    }
+
+    public static int factorial(int a){
+        if(a<0) throw new IllegalArgumentException("Only positive numbers");
+        int result=1;
+        for(int i=2;i<=a;i++) result*=i;
+        return result;
+    }
+
+    public static int sumOfNumbers(int a){
+        a = Math.abs(a);
+        int sum=0;
+        while(a!=0){
+            sum+=a%10;
+            a/=10;
+        }
+        return sum;
+    }
+
+    public static int reverseNumber(Integer a){
+        StringBuilder number = new StringBuilder(a.toString());
+        number.reverse();
+        return Integer.parseInt(number.toString());
+        // Пример с числом, заканчивающимся на 0, все еще будет решен абсолютно зеркально, хотя обрезать
+        // эти нули вначале несложно, но нужно ли - этого я, так-то, не знаю ¯\_(ツ)_/¯
+    }
+
 }
---
+++ Hw_0_FirstHomework/src/com/github/unittests/CyclesTest.java
@@ -0,0 +1,67 @@
+package com.github.unittests;
+
+import com.github.firsthomework.Cycles;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CyclesTest {
+
+    @Test
+    public void sumOfEvenNumber() {
+        Assert.assertEquals(2450, Cycles.sumOfEvenNumber());
+    }
+
+    @Test
+    public void isPrime() {
+        Assert.assertEquals(false,Cycles.isPrime(25));
+        Assert.assertEquals(true,Cycles.isPrime(23));
+    }
+
+    @Test
+    public void squareRootSimple() {
+        Assert.assertEquals(4,Cycles.squareRootSimple(19));
+        Assert.assertEquals(3,Cycles.squareRootSimple(10));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void squareRootSimpleException() {
+        Cycles.squareRootSimple(-10);
+    }
+
+    @Test
+    public void squareRootBinary() {
+        Assert.assertEquals(4,Cycles.squareRootBinary(19));
+        Assert.assertEquals(3,Cycles.squareRootBinary(10));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void squareRootSimpleExceptionException() {
+        Cycles.squareRootBinary(-10);
+    }
+
+    @Test
+    public void factorial() {
+        Assert.assertEquals(120,Cycles.factorial(5));
+        Assert.assertEquals(24, Cycles.factorial(4));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void factorialException() {
+        Cycles.factorial(-1);
+    }
+
+    @Test
+    public void sumOfNumbers() {
+        Assert.assertEquals(12,Cycles.sumOfNumbers(552));
+        Assert.assertEquals(0,Cycles.sumOfNumbers(0));
+    }
+
+    @Test
+    public void reverseNumber() {
+        Assert.assertEquals(123,Cycles.reverseNumber(321));
+        Assert.assertEquals(12,Cycles.reverseNumber(210));
+        Assert.assertEquals(101,Cycles.reverseNumber(101));
+    }
+}
